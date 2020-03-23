55b4576b859f4f303283653c47bb4460531cc749
Till Brede <tb@symcon.de>
2020-03-23 11:40:15 +0100

    added validation test

---
+++ tests/BetriebsstundenzaehlerValidationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/stubs/Validator.php';
+
+class BetriebsstundenzaehlerValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateBetriebsstundenzaehler(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+
+    public function testValidateBetriebsstundenzaehlerModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../Betriebsstundenzaehler');
+    }
+}
