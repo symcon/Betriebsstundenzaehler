d86956b483f8777f81b6efb48427eac9dbd6b0fc
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2021-11-04 15:33:00 +0100

    Fix calculation tests
    - Fix calculation tests
    - Fix aggregated Values

--- tests/BetriebsstundenzaehlerTest.php
+++ tests/BetriebsstundenzaehlerTest.php
@@ -28,15 +28,6 @@ class BetriebsstundenzaehlerTest extends TestBase
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('May 23 1971 00:00:00'),
-            ],
-            [
-                'Avg'       => 0.5,
-                'Duration'  => 24 * 60 * 60,
-                'Max'       => 0,
-                'MaxTime'   => 0,
-                'Min'       => 0,
-                'MinTime'   => 0,
                 'TimeStamp' => strtotime('May 24 1971 00:00:00'),
             ],
             [
@@ -52,7 +43,7 @@ class BetriebsstundenzaehlerTest extends TestBase
         AC_StubsAddAggregatedValues($archivID, $variableID, 1, $aggregationPeriodDay);
 
         //Properties
-        IPS_SetProperty($instanceID, 'Price', 1.0);
+        IPS_SetProperty($instanceID, 'Price', 2.0);
         IPS_SetProperty($instanceID, 'Source', $variableID);
         IPS_SetProperty($instanceID, 'Active', true);
         IPS_SetProperty($instanceID, 'CalculateCost', true);
@@ -62,9 +53,20 @@ class BetriebsstundenzaehlerTest extends TestBase
         //Not use because it calls in ApplyChanges
         //BSZ_Calculate($instanceID);
 
-        $this->assertEquals(0.12, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
-        $this->assertEquals(0.06, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
-        $this->assertEquals(0.12, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+        //Assert
+        $this->assertEquals(6, GetValue(IPS_GetObjectIDByIdent('OperatingHours', $instanceID)));
+        $this->assertEquals(0.24, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
+        $this->assertEquals(0.12, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
+        $this->assertEquals(0.24, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+
+        /*
+        Manuelle Berechnung:
+        Erwartete Daten:
+            Operating Hours:			6h 		(0,5 * 12);
+            Cost Last Period:			0.24 €	(0,5 * 24 *0,02€);
+            Cost This Period:			0.12 €	(6 * 0.02€);
+            Prediction This Period: 	0.24 €	(0,12 /((44020800 - 43977600) / (24*60*60) *100)*100);
+         */
     }
     public function testCalculationWeek()
     {
@@ -92,27 +94,18 @@ class BetriebsstundenzaehlerTest extends TestBase
             ],
             [
                 'Avg'       => 1,
-                'Duration'  => 3 * 24 * 60 * 60,
+                'Duration'  => 3 * 24 * 60 * 60 + 12 * 60 * 60,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
                 'TimeStamp' => strtotime('May 4 2020 00:00:00'),
-            ],
-            [
-                'Avg'       => 1,
-                'Duration'  => 12 * 60 * 60,
-                'Max'       => 0,
-                'MaxTime'   => 0,
-                'Min'       => 0,
-                'MinTime'   => 0,
-                'TimeStamp' => strtotime('May 7 2020 00:00:00'),
             ]
         ];
         AC_StubsAddAggregatedValues($archivID, $variableID, 2, $aggregationPeriodWeek);
 
         //Properties
-        IPS_SetProperty($instanceID, 'Price', 1.0);
+        IPS_SetProperty($instanceID, 'Price', 2.0);
         IPS_SetProperty($instanceID, 'Source', $variableID);
         IPS_SetProperty($instanceID, 'Active', true);
         IPS_SetProperty($instanceID, 'CalculateCost', true);
@@ -124,9 +117,19 @@ class BetriebsstundenzaehlerTest extends TestBase
         //BSZ_Calculate($instanceID);
 
         //Assert
-        $this->assertEquals(0.84, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
-        $this->assertEquals(0.84, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
-        $this->assertEquals(1.68, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+        $this->assertEquals(84, GetValue(IPS_GetObjectIDByIdent('OperatingHours', $instanceID)));
+        $this->assertEquals(1.68, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
+        $this->assertEquals(1.68, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
+        $this->assertEquals(3.36, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+
+        /*
+        Manuelle Berechnung:
+        Erwartete Daten:
+            Operating Hours:			84h		(1* (3 * 24 + 12));
+            Cost Last Period:			1.68 €	(0,5 *(7*24))*0,02€;
+            Cost This Period:			1.68 €	(84 * 0,02€);
+            Prediction This Period: 	3,36 €	(1,68/((1588852800 - 1588550400) / (7 * 24 * 60 * 60) *100)*100);
+         */
     }
     public function testCalculationMonth()
     {
@@ -145,27 +148,27 @@ class BetriebsstundenzaehlerTest extends TestBase
         $aggregationPeriodMonth = [
             [
                 'Avg'       => 0.5,
-                'Duration'  => 7 * 24 * 60 * 60,
+                'Duration'  => 31 * 24 * 60 * 60,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('January 10 2021 00:00:00'),
+                'TimeStamp' => strtotime('January 1 2021 00:00:00'),
             ],
             [
-                'Avg'       => 1,
-                'Duration'  => 7 * 24 * 60 * 60,
+                'Avg'       => 0.5,
+                'Duration'  => 14 * 24 * 60 * 60,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('February 01 2021 00:00:00'),
+                'TimeStamp' => strtotime('February 1 2021 00:00:00'),
             ]
         ];
         AC_StubsAddAggregatedValues($archivID, $variableID, 3, $aggregationPeriodMonth);
 
         //Properties
-        IPS_SetProperty($instanceID, 'Price', 1.0);
+        IPS_SetProperty($instanceID, 'Price', 2.0);
         IPS_SetProperty($instanceID, 'Source', $variableID);
         IPS_SetProperty($instanceID, 'Active', true);
         IPS_SetProperty($instanceID, 'CalculateCost', true);
@@ -177,9 +180,19 @@ class BetriebsstundenzaehlerTest extends TestBase
         //BSZ_Calculate($instanceID);
 
         //Assert
-        $this->assertEquals(1.68, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
-        $this->assertEquals(0.84, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
-        $this->assertEquals(3.36, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+        $this->assertEquals(168, GetValue(IPS_GetObjectIDByIdent('OperatingHours', $instanceID)));
+        $this->assertEquals(3.36, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
+        $this->assertEquals(7.44, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
+        $this->assertEquals(6.72, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+
+        /*
+        Manuelle Berechnung:
+        Erwartete Daten:
+            Operating Hours:			372h    (14 * 24 * 0,5);
+            Cost Last Period:			7.44 €  (0,02 * (31 * 24));
+            Cost This Period:			3.36 €  (372 * 0.02);
+            Prediction This Period: 	6.72 €  (3,36 / ((1613347200 - 1612137600) / (28 * 24 * 60 * 60) * 100) * 100);
+         */
     }
 
     public function testCalculationYear()
@@ -193,42 +206,33 @@ class BetriebsstundenzaehlerTest extends TestBase
         AC_SetLoggingStatus($archivID, $variableID, true);
 
         //Custom Time for Testing
-        BSZ_setTime($instanceID, strtotime('March 29 2021 12:00'));
+        BSZ_setTime($instanceID, strtotime('March 29 2021 13:00'));
 
         //Add archive data
-        $aggregationPeriodMonth = [
+        $aggregationPeriodYear = [
             [
-                'Avg'       => 0.5,
-                'Duration'  => 14 * 24 * 60 * 60,
+                'Avg'       => 0.7,
+                'Duration'  => 366 * 24 * 60 * 60,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('January 10 2021 00:00:00'),
+                'TimeStamp' => strtotime('January 01 2020 00:00:00'),
             ],
             [
-                'Avg'       => 0.5,
-                'Duration'  => 14 * 24 * 60 * 60,
+                'Avg'       => 0.25,
+                'Duration'  => 31 * 24 * 3600 + 28 * 24 * 3600 + 28 * 24 * 3600 + 13 * 3600,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('February 2021 00:00:00'),
-            ],
-            [
-                'Avg'       => 1,
-                'Duration'  => 28 * 24 * 60 * 60,
-                'Max'       => 0,
-                'MaxTime'   => 0,
-                'Min'       => 0,
-                'MinTime'   => 0,
-                'TimeStamp' => strtotime('February 01 2020 00:00:00'),
+                'TimeStamp' => strtotime('January 1 2021 00:00:00'),
             ]
         ];
-        AC_StubsAddAggregatedValues($archivID, $variableID, 4, $aggregationPeriodMonth);
+        AC_StubsAddAggregatedValues($archivID, $variableID, 4, $aggregationPeriodYear);
 
         //Properties
-        IPS_SetProperty($instanceID, 'Price', 1.0);
+        IPS_SetProperty($instanceID, 'Price', 2.0);
         IPS_SetProperty($instanceID, 'Source', $variableID);
         IPS_SetProperty($instanceID, 'Active', true);
         IPS_SetProperty($instanceID, 'CalculateCost', true);
@@ -240,9 +244,19 @@ class BetriebsstundenzaehlerTest extends TestBase
         //BSZ_Calculate($instanceID);
 
         //Assert
-        $this->assertEquals(3.36, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
-        $this->assertEquals(6.72, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
-        $this->assertEquals(14.016, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+        $this->assertEquals(525.25, GetValue(IPS_GetObjectIDByIdent('OperatingHours', $instanceID)));
+        $this->assertEquals(10.505, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
+        $this->assertEquals(122.976, GetValue(IPS_GetObjectIDByIdent('CostLastPeriod', $instanceID)));
+        $this->assertEquals(43.8, GetValue(IPS_GetObjectIDByIdent('PredictionThisPeriod', $instanceID)));
+
+        /*
+        Manuelle Berechnung:
+        Erwartete Daten:
+            Operating Hours:			525,25h     ((31 * 24 + 28 * 24 + 28 * 24 + 12 +1) * 0,25)
+            Cost Last Period:			122,976 €   ((366 * 24) * 0,07);
+            Cost This Period:			10.505 €    (525,25 * 0,02);
+            Prediction This Period: 	43,8 €      (10,505 / ((1617022800 - 1609459200) / (31536000) * 100) * 100);
+         */
     }
     public function testCalculationComplete()
     {
@@ -258,39 +272,39 @@ class BetriebsstundenzaehlerTest extends TestBase
         BSZ_setTime($instanceID, strtotime('March 29 2021 12:00'));
 
         //Add archive data
-        $aggregationPeriodMonth = [
+        $aggregationPeriodComplete = [
             [
-                'Avg'       => 0.5,
-                'Duration'  => 14 * 24 * 60 * 60,
+                'Avg'       => 0.3,
+                'Duration'  => 365 * 24 * 60 * 60,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('January 10 2021 00:00:00'),
+                'TimeStamp' => strtotime('January 1 1998 00:00:00'),
             ],
             [
                 'Avg'       => 0.5,
-                'Duration'  => 14 * 24 * 60 * 60,
+                'Duration'  => 365 * 24 * 60 * 60,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('February 2021 00:00:00'),
+                'TimeStamp' => strtotime('January 1 2020 00:00:00'),
             ],
             [
-                'Avg'       => 1,
-                'Duration'  => 28 * 24 * 60 * 60,
+                'Avg'       => 0.5,
+                'Duration'  => 87 * 24 * 60 * 60,
                 'Max'       => 0,
                 'MaxTime'   => 0,
                 'Min'       => 0,
                 'MinTime'   => 0,
-                'TimeStamp' => strtotime('February 24 1998 00:00:00'),
+                'TimeStamp' => strtotime('January 1 2021 00:00:00'),
             ]
         ];
-        AC_StubsAddAggregatedValues($archivID, $variableID, 4, $aggregationPeriodMonth);
+        AC_StubsAddAggregatedValues($archivID, $variableID, 4, $aggregationPeriodComplete);
 
         //Properties
-        IPS_SetProperty($instanceID, 'Price', 1.0);
+        IPS_SetProperty($instanceID, 'Price', 2.0);
         IPS_SetProperty($instanceID, 'Source', $variableID);
         IPS_SetProperty($instanceID, 'Active', true);
         IPS_SetProperty($instanceID, 'CalculateCost', true);
@@ -302,6 +316,14 @@ class BetriebsstundenzaehlerTest extends TestBase
         //BSZ_Calculate($instanceID);
 
         //Assert
-        $this->assertEquals(10.08, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
+        $this->assertEquals(8052, GetValue(IPS_GetObjectIDByIdent('OperatingHours', $instanceID)));
+        $this->assertEquals(161.04, GetValue(IPS_GetObjectIDByIdent('CostThisPeriod', $instanceID)));
+
+        /*
+        Manuelle Berechnung:
+        Erwartete Daten:
+            Operating Hours:			8052h       (365 * 24 * 0,3 + 365 * 24 * 0,5 + 87 * 24 * 0,5);
+            Cost This Period:			161,04 €    (8052 * 0,02);
+         */
     }
 }
